c2da2102f57ba733adef97f72f8dd86cbf269d06
ravikumar <ravikumar@preceptlabs.com>
2020-11-01 17:46:46 +0530

    created cart-item component and connected cart dropdown with cart-item reducer

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -8,7 +8,23 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
       ...existingCartItem,
       quantity: existingCartItem.quantity + 1,
     };
-    return cartItems;
+    console.log("items ===>>", cartItems);
+    return [...cartItems];
   }
   return cartItems.concat({ ...cartItemToAdd, quantity: 1 });
 };
+
+// export const addItemToCart = (cartItems, cartItemToAdd) => {
+//     const existingCartItem = cartItems.find(
+//       (item) => item.id === cartItemToAdd.id
+//     );
+
+//     if (existingCartItem) {
+//       return cartItems.map((cartItem) =>
+//         cartItem.id === cartItemToAdd.id
+//           ? { ...cartItem, quantity: cartItem.quantity + 1 }
+//           : cartItem
+//       );
+//     }
+//     return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+//   };
